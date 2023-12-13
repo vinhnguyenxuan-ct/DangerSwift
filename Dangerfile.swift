9f79b364e9c94965c5709b9fb7109d6c8b3cc45e
Vinh Nguyen <109069354+vinhnguyenxuan-ct@users.noreply.github.com>
2023-12-13 11:53:20 +0700

    Update Dangerfile

--- Dangerfile.swift
+++
@@ -1,5 +0,0 @@
-import Danger 
-
-let danger = Danger()
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined(separator: ", "))")
