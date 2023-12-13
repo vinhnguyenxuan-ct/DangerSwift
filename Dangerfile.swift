5c03eb8c1658abb1e951e9c89c7e6792d08e1b41
Vinh Nguyen <109069354+vinhnguyenxuan-ct@users.noreply.github.com>
2023-12-13 12:26:23 +0700

    Update

---
+++ Dangerfile.swift
@@ -0,0 +1,31 @@
+import Danger
+import Foundation
+
+let danger = Danger()
+
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
+    $0.fileType == .swift
+        && danger.utils.readFile($0).contains("//  Created by")
+}
+
+if !swiftFilesWithCopyright.isEmpty {
+    let files = swiftFilesWithCopyright.joined(separator: ", ")
+    warn("In Danger JS we don't include copyright headers, found them in: \(files)")
+}
+
+let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles).filter { !$0.contains("Documentation/") }
+
+SwiftLint.lint(.files(filesToLint), inline: true)
+
+// Support running via `danger local`
+if danger.github != nil {
+    // These checks only happen on a PR
+    if danger.github.pullRequest.title.contains("WIP") {
+        warn("PR is classed as Work in Progress")
+    }
+}
