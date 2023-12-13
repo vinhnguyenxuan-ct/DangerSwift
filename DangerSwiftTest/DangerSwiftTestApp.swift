2572ef0090413e570ba890e5d498ee871fb5f3f2
Vinh Nguyen <109069354+vinhnguyenxuan-ct@users.noreply.github.com>
2023-12-13 10:46:11 +0700

    Add files

---
+++ DangerSwiftTest/DangerSwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerSwiftTestApp.swift
+//  DangerSwiftTest
+//
+//  Created by Vinh Nguyen Xuan on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DangerSwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DangerSwiftTestUITests/DangerSwiftTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DangerSwiftTestUITestsLaunchTests.swift
+//  DangerSwiftTestUITests
+//
+//  Created by Vinh Nguyen Xuan on 13/12/2023.
+//
+
+import XCTest
+
+final class DangerSwiftTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
