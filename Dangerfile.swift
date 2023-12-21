2af1cc4c64a4a305cd93986b422b915bab6bb740
Vinh Nguyen <109069354+vinhnguyenxuan-ct@users.noreply.github.com>
2023-12-21 10:28:15 +0700

    Update danger files

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,28 +5,39 @@ let danger = Danger()
 
 // More example on: https://gist.github.com/candostdagdeviren/e49271e6a4b80f93f3193af89d10f4b1
 
-
-if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
-    warn("Big PR, try to keep changes smaller if you can")
-}
-
-let swiftFilesWithCopyright = danger.git.createdFiles.filter {
-    $0.fileType == .swift
-    && danger.utils.readFile($0).contains("//  Created by")
-}
-
-if !swiftFilesWithCopyright.isEmpty {
-    let files = swiftFilesWithCopyright.joined(separator: ", ")
-    warn("In Danger JS we don't include copyright headers, found them in: \(files)")
-}
-
+// * run SwiftLint
 let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles).filter { !$0.contains("Documentation/") }
 SwiftLint.lint(.files(filesToLint), inline: true)
 
-// Support running via `danger local`
+// * Warn if big changes
+let maxFilesCount = 100
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > maxFilesCount {
+    warn("Big PR, try to keep changes smaller if you can, currently over \(maxFilesCount)")
+}
+
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
+    $0.fileType == .swift && danger.utils.readFile($0).contains("//  Created by")
+}
+
+// * Check missing header copyright
+if swiftFilesWithCopyright.isEmpty {
+    let files = swiftFilesWithCopyright.joined(separator: ", ")
+    warn("Missing header copyright, found them in: \(files). Please check again")
+}
+
+// * Support running via `danger local`
 if danger.github != nil {
     // These checks only happen on a PR
     if danger.github.pullRequest.title.contains("WIP") {
         warn("PR is classed as Work in Progress")
     }
+}
+
+let hasAppChanges = !git.modifiedFiles.filter { $0.contains("DangerSwiftTest/") }.isEmpty
+let hasTestChanges = !git.modifiedFiles.filter { $0.contains("DangerSwiftTestTets/") }.isEmpty
+
+// * If files changes, tests need to be updated too
+if hasAppChanges && !hasTestChanges {
+    // Assuming you have a function similar to the Ruby 'fail' function
+    warn("Tests were not updated")
 }
