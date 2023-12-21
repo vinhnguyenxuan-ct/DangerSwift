f9eb14016e460540d8c30137bd26b6707eee243e
Vinh Nguyen <109069354+vinhnguyenxuan-ct@users.noreply.github.com>
2023-12-21 10:24:27 +0700

    Update Danger file

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,41 +2,31 @@ import Danger
 import Foundation
 
 let danger = Danger()
-let git = danger.git
-let github = danger.github
 
 // More example on: https://gist.github.com/candostdagdeviren/e49271e6a4b80f93f3193af89d10f4b1
 
-if git.createdFiles.count + git.modifiedFiles.count - git.deletedFiles.count > 300 {
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-let swiftFilesWithCopyright = git.createdFiles.filter {
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
     $0.fileType == .swift
     && danger.utils.readFile($0).contains("//  Created by")
 }
 
-if swiftFilesWithCopyright.isEmpty {
+if !swiftFilesWithCopyright.isEmpty {
     let files = swiftFilesWithCopyright.joined(separator: ", ")
-    warn("Missing header copyright, found them in: \(files). Please check again")
+    warn("In Danger JS we don't include copyright headers, found them in: \(files)")
 }
 
-let filesToLint = (git.modifiedFiles + git.createdFiles).filter { !$0.contains("Documentation/") }
+let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles).filter { !$0.contains("Documentation/") }
 SwiftLint.lint(.files(filesToLint), inline: true)
 
 // Support running via `danger local`
-if github != nil {
+if danger.github != nil {
     // These checks only happen on a PR
-    if github.pullRequest.title.contains("WIP") {
+    if danger.github.pullRequest.title.contains("WIP") {
         warn("PR is classed as Work in Progress")
     }
-}
-
-let hasAppChanges = !git.modifiedFiles.filter { $0.contains("DangerSwiftTest/") }.isEmpty
-let hasTestChanges = !git.modifiedFiles.filter { $0.contains("DangerSwiftTestTets/") }.isEmpty
-
-// If changes are more than 10 lines of code, tests need to be updated too
-if hasAppChanges && !hasTestChanges {
-    // Assuming you have a function similar to the Ruby 'fail' function
-    warn("Tests were not updated")
 }
